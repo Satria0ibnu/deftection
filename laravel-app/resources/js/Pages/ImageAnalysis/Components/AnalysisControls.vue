119eeb8aefd8daec1a0248f12ccc09c96838adfc
Wilsent Philip <wphilip465@gmail.com>
2025-08-07 23:49:30 +0700

    remove sensitivity and threat checker option on singular image analysis

--- laravel-app/resources/js/Pages/ImageAnalysis/Components/AnalysisControls.vue
+++ laravel-app/resources/js/Pages/ImageAnalysis/Components/AnalysisControls.vue
@@ -17,40 +17,6 @@ const emit = defineEmits(["runAnalysis"]);
 
 <template>
     <div class="pt-3">
-        <!-- Sensitivity Slider -->
-        <div class="mb-3">
-            <label
-                for="sensitivity"
-                class="flex items-center gap-2 text-sm font-medium text-gray-800 dark:text-dark-100"
-            >
-                Sensitivity: <span class="font-bold">{{ sensitivity }}</span>
-            </label>
-            <input
-                id="sensitivity"
-                type="range"
-                min="0.1"
-                max="1.0"
-                step="0.1"
-                v-model="sensitivity"
-                class="my-2 form-range this:primary text-this dark:text-this-light"
-            />
-
-            <div
-                class="mt-1 flex justify-between text-xs text-gray-800 dark:text-dark-100"
-            >
-                <span>0.1 (Low)</span>
-                <span>1.0 (High)</span>
-            </div>
-        </div>
-
-        <!-- Options -->
-        <div class="space-y-2">
-            <label class="flex items-center space-x-2">
-                <input type="checkbox" v-model="options.threatChecker" />
-                <span class="text-sm">Threat checker</span>
-            </label>
-        </div>
-
         <!-- Run Analysis Button -->
         <button
             @click="emit('runAnalysis', { sensitivity, ...options })"
