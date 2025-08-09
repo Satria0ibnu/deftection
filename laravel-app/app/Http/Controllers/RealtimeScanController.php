985b3ad6fd7859bbbcc10d36b1bb150b896bfb86
Wilsent Philip <wphilip465@gmail.com>
2025-08-09 22:09:19 +0700

    add: return inertia render page for DetailSession/Index

--- laravel-app/app/Http/Controllers/RealtimeScanController.php
+++ laravel-app/app/Http/Controllers/RealtimeScanController.php
@@ -2,9 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use Inertia\Inertia;
 use Illuminate\Http\Request;
 
 class RealtimeScanController extends Controller
 {
     //
+    public function index()
+    {
+        // This method can be used to list all realtime scans if needed
+        return Inertia::render('DetailSession/Index');
+    }
 }
