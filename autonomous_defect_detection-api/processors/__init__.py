b5471cd69ad89149fd371d8e1e57d0adb8cca711
satria0ibnu <satria0ibnu@gmail.com>
2025-08-11 23:12:25 +0700

    remove other folder

--- autonomous_defect_detection-api/processors/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Processors package for Unified Defect Detection System
-"""
-
-from .image_processor import ImageProcessor
-from .video_processor import VideoProcessor
-from .realtime_processor import RealTimeProcessor
-
-__all__ = ['ImageProcessor', 'VideoProcessor', 'RealTimeProcessor']
