e2206caf77d7c5e30570cab48a68c4eb9dd79fdf
satria0ibnu <satria0ibnu@gmail.com>
2025-08-17 05:12:53 +0700

    fix DatabaseSeeder

--- laravel-app/database/seeders/DatabaseSeeder.php
+++ laravel-app/database/seeders/DatabaseSeeder.php
@@ -20,34 +20,36 @@ class DatabaseSeeder extends Seeder
             // DefectTypeSeeder::class,
             // MockSeeder::class,
 
-            User::factory(3)->create()->each(
-                function ($user, $index) {
-                    if ($index == 0) {
-                        $user->update([
-                            'name' => 'admin ' . $index,
-                            'role' => 'admin',
-                            'email' => 'admin@example.com',
-                            'password' => bcrypt('123456'),
-                        ]);
-                    }
-                    if ($index == 1) {
-                        $user->update([
-                            'name' => 'user ' . $index,
-                            'role' => 'user',
-                            'email' => 'user@example.com',
-                            'password' => bcrypt('123456'),
-                        ]);
-                    }
-                    if ($index == 2) {
-                        $user->update([
-                            'name' => 'admin ' . $index,
-                            'role' => 'admin',
-                            'email' => 'admin2@example.com',
-                            'password' => bcrypt('123456'),
-                        ]);
-                    }
-                }
-            )
+
         ]);
+
+        User::factory(3)->create()->each(
+            function ($user, $index) {
+                if ($index == 0) {
+                    $user->update([
+                        'name' => 'admin ' . $index,
+                        'role' => 'admin',
+                        'email' => 'admin@example.com',
+                        'password' => bcrypt('123456'),
+                    ]);
+                }
+                if ($index == 1) {
+                    $user->update([
+                        'name' => 'user ' . $index,
+                        'role' => 'user',
+                        'email' => 'user@example.com',
+                        'password' => bcrypt('123456'),
+                    ]);
+                }
+                if ($index == 2) {
+                    $user->update([
+                        'name' => 'admin ' . $index,
+                        'role' => 'admin',
+                        'email' => 'admin2@example.com',
+                        'password' => bcrypt('123456'),
+                    ]);
+                }
+            }
+        );
     }
 }
